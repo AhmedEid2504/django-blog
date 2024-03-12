63ab6b951737677a86612792e791baade2355dcd
Ahmed Eid <ahmedeid2504@gmail.com>
2024-03-12 20:21:01 +0200

    adds styling and functioinality to view each post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from . models import Post
 # Create your views here.
+def index(request):
+    posts = Post.objects.order_by('created_at') #get the latest post at the top of the page
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+    post = Post.objects.get(id = pk)
+    return render(request, 'post.html', {'post': post})
